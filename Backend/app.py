a9cac8ee80ec39319b238397c3573c6980ee4135
urwahfarooq <urwahfarooq303@gmail.com>
2026-06-07 15:15:53 +0500

    Fix vercel deployment

--- Backend/app.py
+++ Backend/app.py
@@ -58,4 +58,4 @@ def predict_dl():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
